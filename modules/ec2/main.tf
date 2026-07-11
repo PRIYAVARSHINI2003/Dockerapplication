a0c3f0af2b3d5785bdc08a83b0219c5dd09c32d3
PRIYAVARSHINI2003 <vvarshini833@gmail.com>
2026-07-11 20:35:54 +0530

    wrong commit

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "this" {
   # Create one instance per security group id (if you want a single instance, remove for_each)
   for_each = toset(var.security_groups)
 
-  security_groups = [each.value]
+  security_groups = [eac.value]
   instance_type   = "t2.micro"
   subnet_id       = var.subnet_id
   ami              = data.aws_ami.this.id

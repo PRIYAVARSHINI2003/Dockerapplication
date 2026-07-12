b27356f0f1477a30859d4973c0ca320b57fe0d77
PRIYAVARSHINI2003 <vvarshini833@gmail.com>
2026-07-12 19:27:21 +0530

    moving changes

--- modules/codepipeline/iamrole.tf
+++ modules/codepipeline/iamrole.tf
@@ -1,31 +1,51 @@
-resource "aws_iam_role" "codepipeline_role"{
-    name="codepipeline-role"
-    assume_role_policy=jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                 Action = [
+resource "aws_iam_role" "codepipeline_role" {
+  name = "codepipeline-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "codepipeline.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "codepipeline_codebuild_policy" {
+  name = "codepipeline-codebuild-policy"
+
+  role = aws_iam_role.codepipeline_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
           "codebuild:StartBuild",
           "codebuild:BatchGetBuilds"
         ]
+
         Resource = var.project_arn
-                Effect = "Allow"
-                Principal = {
-                    Service = "codepipeline.amazonaws.com"
-                }
-            },
-        ]
-    })
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "codepipeline_role_policy_attachment" {
   role       = aws_iam_role.codepipeline_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSCodePipeline_FullAccess"
 }
+
 resource "aws_iam_role_policy_attachment" "codepipeline_s3" {
-
-  role = aws_iam_role.codepipeline_role.name
-
+  role       = aws_iam_role.codepipeline_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-
 }

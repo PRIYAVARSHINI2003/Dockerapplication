20b8c549679cdac0b658bf0d81c03141e011335c
PRIYAVARSHINI2003 <vvarshini833@gmail.com>
2026-07-12 19:42:22 +0530

    iam changes

--- modules/codebuild/iamrole.tf
+++ modules/codebuild/iamrole.tf
@@ -1,24 +1,88 @@
-resource aws_iam_role "codebuild_role"{
-    name = "codebuild-role"
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Action = "sts:AssumeRole"
-                Effect = "Allow"
-                Principal = {
-                    Service = "codebuild.amazonaws.com"
-                }
-            },
-        ]
-    })
+resource "aws_iam_role" "codebuild_role" {
+  name = "codebuild-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "codebuild.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
-resource "aws_iam_role_policy_attachment" "codebuild_role_policy_attachment" {
-  role       = aws_iam_role.codebuild_role.name
+
+# Allows CodeBuild to push/pull Docker images from ECR
+resource "aws_iam_role_policy_attachment" "codebuild_ecr" {
+
+  role = aws_iam_role.codebuild_role.name
+
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
 }
-resource "aws_iam_role_policy_attachment" "codebuild_service_role" {
-  role       = aws_iam_role.codebuild_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
+
+
+# Allows CodeBuild to write build logs to CloudWatch
+resource "aws_iam_role_policy" "codebuild_logs" {
+
+  name = "codebuild-cloudwatch-logs"
+
+  role = aws_iam_role.codebuild_role.id
+
+  policy = jsonencode({
+
+    Version = "2012-10-17"
+
+    Statement = [
+
+      {
+        Effect = "Allow"
+
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+
+        Resource = "*"
+      }
+
+    ]
+  })
+}
+
+
+# Allows CodeBuild to access S3 artifacts from CodePipeline
+resource "aws_iam_role_policy" "codebuild_s3" {
+
+  name = "codebuild-s3-access"
+
+  role = aws_iam_role.codebuild_role.id
+
+  policy = jsonencode({
+
+    Version = "2012-10-17"
+
+    Statement = [
+
+      {
+        Effect = "Allow"
+
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:PutObject"
+        ]
+
+        Resource = "*"
+      }
+
+    ]
+  })
 }
